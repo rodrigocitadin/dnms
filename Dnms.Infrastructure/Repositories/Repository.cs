532ce1abf8ed4c55cd2c09ce128a71d6aef910be
rodrigocitadin <citadinrodrigo@gmail.com>
2025-04-13 03:30:07 -0300

    feat: generalization of repository

---
+++ Dnms.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Dnms.Core.Interfaces;
+
+namespace Dnms.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+    public async Task<T?> GetByIdAsync() => await _dbSet.FindAsync();
+}
